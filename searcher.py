852ab2e4ac29b50b38026c4fc2d1aa5314237b7a
TakuyaUshiroshoji <usirotakuya1236@gmail.com>
2025-11-25 23:37:33 +0900

    大文字小文字区別なく検索できるように追加

--- searcher.py
+++ searcher.py
@@ -2,7 +2,7 @@ def search_items(items, keyword):
     """"itemsの中からkeywordで検索して結果を返す"""
     results = []
     for item in items:
-        if keyword in item:
+        if keyword.lower() in item.lower():
             results.append(item)
 
     return results
