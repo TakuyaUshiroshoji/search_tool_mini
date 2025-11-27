c7868fa22db5dc9fbc3c1f8fd595d2fd350ba704
TakuyaUshiroshoji <usirotakuya1236@gmail.com>
2025-11-27 23:20:56 +0900

    AND検索の実装

--- search_tool_main.py
+++ search_tool_main.py
@@ -1,12 +1,12 @@
 from loader import load_items
-from searcher import search_items
+from searcher import or_search_items, and_search_items
 from writter import save_results
 
 def main():
     items = load_items('data.txt')
     keywords = []
     keywords = (input("検索したいワードを入力してください : ").split())
-    hits = search_items(items, keywords)
+    hits = and_search_items(items, keywords)
     print(len(hits), "件ヒットしました。")
     save_results('search_result.json', keywords, hits)
 
--- searcher.py
+++ searcher.py
@@ -1,9 +1,19 @@
-def search_items(items, keywords):
+def or_search_items(items, keywords):
     """"itemsの中からkeywordsでOR検索して結果を返す"""
     results = []
     for item in items:
         lower_item = item.lower()
 
         if any(kw.lower() in lower_item for kw in keywords):
+            results.append(item)
+    return results
+
+def and_search_items(items, keywords):
+    """"itemsの中からkeywordsでAND検索して結果を返す"""
+    results = []
+    for item in items:
+        lower_item = item.lower()
+
+        if all(kw.lower() in lower_item for kw in keywords):
             results.append(item)
     return results
