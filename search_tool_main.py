0ef73b79390e6d0baeb4a3217af9c7deb0988350
TakuyaUshiroshoji <usirotakuya1236@gmail.com>
2025-12-03 22:57:19 +0900

    コマンドライン引数対応

--- search_tool_main.py
+++ search_tool_main.py
@@ -2,21 +2,36 @@ from loader import load_items
 from searcher import or_search_items, and_search_items
 from writter_json import save_json
 from writter_csv import save_csv
-from select import select
+
+import argparse
 
 def main():
-    items = load_items('data.txt')
-    keywords = (input("検索したいワードを入力してください : ").split())
+    parser = argparse.ArgumentParser(description="検索ツール")
+    parser.add_argument(
+        "--mode",
+        choices=["AND", "OR"],
+        default="AND",
+        help="AND検索 or OR検索を選択"
+        )
     
-    number = select()
-    if number == 1:
-        hits = or_search_items(items, keywords)
-    elif number == 2:
-        hits = and_search_items(items, keywords)
+    parser.add_argument(
+        "--keywords",
+        required=True,
+        nargs="+",
+        help="検索キーワード(複数可)"
+        )
+    
+    args = parser.parse_args()
+    print(args.mode)
+    print(args.keywords)
+
+    items = load_items('data.txt')
+    search_func = and_search_items if args.mode == "AND" else or_search_items
+    hits = search_func(items, args.keywords)
 
     print(len(hits), "件ヒットしました。")
-    save_json('search_result.json', keywords, hits)
-    save_csv('search_result.csv', keywords, hits)
+    save_json('search_result.json', args.keywords, hits)
+    save_csv('search_result.csv', args.keywords, hits)
 
 if __name__ == "__main__":
     main()
